a8807a51ae9e67f6ac03517ccd96591f6c02e7ea
Jose Franz Lita <user@example.com>
2026-04-13 03:17:20 -0400

    Add dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/life-is-strange-2/',
   server: {
-    allowedHosts: ['*'],
     host: '0.0.0.0',
-  },
+    port: 5173
+  }
 })
